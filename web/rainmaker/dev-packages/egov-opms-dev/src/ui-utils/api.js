04c72c989e476d803486d2ec452d91def6f406de
extrapre <extrapre>
2020-04-30 11:37:35 +0530

    EPI OPMS Updated code Commited

--- web/rainmaker/dev-packages/egov-opms-dev/src/ui-utils/api.js
+++ web/rainmaker/dev-packages/egov-opms-dev/src/ui-utils/api.js
@@ -17,8 +17,7 @@ import {
 } from "egov-ui-kit/utils/localStorageUtils";
 
 const instance = axios.create({
-  //baseURL: window.location.origin,
-  baseURL: 'http://192.168.12.116:8096',
+  baseURL: window.location.origin,
   headers: {
     "Content-Type": "application/json"
   }
