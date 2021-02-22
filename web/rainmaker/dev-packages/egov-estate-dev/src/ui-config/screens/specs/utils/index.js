3e07525752e1d20e2336769493dcb7fc19d53a63
priyankatalkar <priyanka.talkar@transerve.com>
2021-02-22 12:57:35 +0530

    added error messages in payment page

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/utils/index.js
@@ -2359,7 +2359,7 @@ export const _getPattern = (type) => {
         case "variationdetail":
             return /^([\s\S]){0,150}$/i;
             case "street":
-              return /^[a-zA-Z0-9]{2,100}$/i;
+              return /^[a-zA-Z0-9 ]{2,100}$/i;
               case "height":
       return /^[1-9][0-9]{0,6}$/i;
       case "numeric":
@@ -2369,7 +2369,11 @@ export const _getPattern = (type) => {
         case "HouseNumber":
           return /^[1-9a-zA-Z][\s\S]{1,49}$/i;
           case "transactionid":
-            return /^[1-9a-zA-Z][0-9a-zA-Z]{1,249}$/i;
+            return /^[1-9a-zA-Z][0-9a-zA-Z ]{1,249}$/i;
+            case "Amount":
+              return /^[1-9][0-9]{1,7}$/i;
+              case "BankName":
+                return /^[a-zA-Z ]{1,250}$/i;
   }
 }
 
