f04ff429f60cf34aefec25471c394a48b3fbc9cb
shravandeshpande1508 <shravan.deshpande@extrapreneursindia.com>
2021-03-03 13:50:45 +0530

    WNS dashboard

---
+++ web/rainmaker/dev-packages/egov-dashboard-dev/src/ui-config/screens/specs/egov-dashboard/WNSDashboard.js
@@ -0,0 +1,104 @@
+
+import { getBreak, getCommonHeader } from "egov-ui-framework/ui-config/screens/specs/utils";
+import { prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import { getUserInfo, setapplicationType } from "egov-ui-kit/utils/localStorageUtils";
+import { getDashboardDropdownData } from "../../../../ui-utils/commons";
+// import { HCDashboardFilterForm, HCDashboardResults } from "./HCDashboard/HCDashboard";
+import { FilterForm, DashboardResults } from "./WNSDashboard/WNSDashboard";
+import './index.css';
+
+let role_name = JSON.parse(getUserInfo()).roles[0].code
+
+const header = getCommonHeader(
+  {
+    labelName: "WNS Dashboard",
+    labelKey: "WNS_dashboard_1"
+  },
+  {
+    classes: {
+      root: "common-header-cont"
+    },
+    style: {
+      padding : "8px"
+    },
+  }
+);
+
+const defaultDate = (date) => {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+const getDropDownData = async (action, state, dispatch) => {
+
+  debugger
+//   let data = getDashboardDropdownData(state, dispatch, status)
+  var data =  [
+    {
+    "name" : "Dahboard 1",
+    "code" : "dashboardType1"
+    },
+    {
+      "name" : "Dashboard 2",
+      "code" : "dashboardType2"
+    }
+  ]
+  var selectedDefaultData = {value: "dashboardType1", label: "Dahboard 1"};
+
+  // Date default
+  var fromDate = new Date();
+  var formatDt = defaultDate(fromDate);
+
+  dispatch(prepareFinalObject("dahsboardHome.dropDownData", data));
+  dispatch(prepareFinalObject("dahsboardHome.dropDownData2", selectedDefaultData));
+  dispatch(prepareFinalObject("dahsboardHome.defaultFromDate", formatDt));
+  dispatch(prepareFinalObject("dahsboardHome.defaulttoDate", formatDt));
+}
+
+const WNSDashboard = {
+  uiFramework: "material-ui",
+  name: "WNSDashboard",
+  beforeInitScreen: (action, state, dispatch) => {
+    
+    debugger
+    getDropDownData(action, state, dispatch);
+    return action;
+  },
+  components: {
+    div: {
+      uiFramework: "custom-atoms",
+      componentPath: "Form",
+      props: {
+        className: "common-div-css",
+        id: "dashboardReportFilter"
+      },
+      children: {
+        headerDiv: {
+          uiFramework: "custom-atoms",
+          componentPath: "Container",
+
+          children: {
+            header: {
+              ...header
+            },
+            
+          }
+        },
+        FilterForm,
+        breakAfterSearch: getBreak(),
+        DashboardResults
+      }
+    },
+  }
+};
+
+export default WNSDashboard;
