fb166d530ece455e160f339f1f7433382058685a
Ruchira Naik <ruchira.naik@transerve.com>
2021-03-08 14:05:42 +0530

    updated function downloadAckForm to handle wf documents

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/applyResource/footer.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/applyResource/footer.js
@@ -1546,6 +1546,7 @@ export const downloadPrintContainer = (
     link: () => {
       const { Applications,temp } = state.screenConfiguration.preparedFinalObject;
       const documents = temp[0].reviewDocData;
+      const wfDocuments = temp[0].reviewWfDocData ? temp[0].reviewWfDocData : []
       let { applicationType} = Applications[0];
       const {branchType} = Applications[0];
       if(branchType === "BuildingBranch"){
@@ -1555,6 +1556,7 @@ export const downloadPrintContainer = (
         applicationType =  "MM-" + applicationType 
        }
       set(Applications[0],"additionalDetails.documents",documents)
+      set(Applications[0],"additionalDetails.wfDocuments",wfDocuments)
       const feeEstimate = temp[0].estimateCardData;
       downloadAcknowledgementForm(Applications,applicationType,feeEstimate,applicationState);
     },
@@ -1713,6 +1715,7 @@ export const downloadPrintContainer = (
     link: () => {
       const { Applications,temp } = state.screenConfiguration.preparedFinalObject;
       const documents = temp[0].reviewDocData;
+      const wfDocuments = temp[0].reviewWfDocData ? temp[0].reviewWfDocData : []
       let { applicationType} = Applications[0];
       const {branchType} = Applications[0];
       if(branchType === "BuildingBranch"){
@@ -1722,6 +1725,7 @@ export const downloadPrintContainer = (
         applicationType =  "MM-" + applicationType 
        }
       set(Applications[0],"additionalDetails.documents",documents)
+      set(Applications[0],"additionalDetails.wfDocuments",wfDocuments)
       const feeEstimate = temp[0].estimateCardData;
       downloadAcknowledgementForm(Applications,applicationType,feeEstimate,applicationState,'print');    },
     leftIcon: "assignment"
@@ -2068,7 +2072,7 @@ export const downloadPrintContainer = (
              applicationDownloadObject
            ]
            printMenu = [
-               applicationPrintObject
+              applicationPrintObject
           ]
       break;
 
