ef51507adc6ec5236905de827db641ed38847fcf
shravandeshpande1508 <shravan.deshpande@extrapreneursindia.com>
2021-08-23 10:06:58 +0530

    Sellmeat nocCoughtfor Change

--- web/rainmaker/dev-packages/egov-opms-dev/src/ui-config/screens/specs/egov-opms/applysellmeat.js
+++ web/rainmaker/dev-packages/egov-opms-dev/src/ui-config/screens/specs/egov-opms/applysellmeat.js
@@ -225,7 +225,7 @@ const getMdmsData = async (action, state, dispatch) => {
   }
 };
 
-export const prepareEditFlow = async (state, dispatch, applicationNumber, tenantId, step) => {
+export const prepareEditFlow = async (action, state, dispatch, applicationNumber, tenantId, step) => {
   if (applicationNumber) {
     let response = await getSearchResultsView([
       { key: "tenantId", value: tenantId },
@@ -234,6 +234,21 @@ export const prepareEditFlow = async (state, dispatch, applicationNumber, tenant
     let Refurbishresponse = furnishSellMeatNocResponse(state,response, step);
 
     dispatch(prepareFinalObject("SELLMEATNOC", Refurbishresponse));
+
+    let statusPAID = Refurbishresponse.statusPAID;
+    if(statusPAID){
+      set(
+        action.screenConfig,
+        "components.div.children.formwizardFirstStep.children.nocDetails.children.cardContent.children.nocDetailsContainer.children.nocSought.props.disabled",
+        true
+      );
+      set(
+        action.screenConfig,
+        "components.div.children.formwizardFirstStep.children.nocDetails.children.cardContent.children.nocDetailsContainer.children.nocSought.props.disabled",
+        true
+      );
+    }
+
     if (applicationNumber) {
       setapplicationNumber(applicationNumber);
       setApplicationNumberBox(state, dispatch, applicationNumber);
@@ -562,8 +577,21 @@ const screenConfig = {
       // Set Documents Data (TEMP)
       prepareDocumentsUploadData(state, dispatch, 'apply_sellmeat');
     // Search in case of EDIT flow
-    prepareEditFlow(state, dispatch, applicationNumber, tenantId, step);
+    prepareEditFlow(action, state, dispatch, applicationNumber, tenantId, step);
 
+    let statusPAID = get(state, "screenConfiguration.preparedFinalObject.SELLMEATNOC.statusPAID", false);
+    if(statusPAID){
+      set(
+        action.screenConfig,
+        "components.div.children.formwizardFirstStep.children.nocDetails.children.cardContent.children.nocDetailsContainer.children.nocSought.props.disabled",
+        true
+      );
+      set(
+        action.screenConfig,
+        "components.div.children.formwizardFirstStep.children.nocDetails.children.cardContent.children.nocDetailsContainer.children.nocSought.props.disabled",
+        true
+      );
+    }
     // Clear Undertakking dropdown 
     // dispatch(prepareFinalObject("SELLMEATNOC.nocSought", ""));
     dispatch(prepareFinalObject("dropdownRead", false));
